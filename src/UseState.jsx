b7a5a2e334d7b4f498a196c6246f327e323ab4cd
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-03-27 16:37:40 -0500

    6.Estados independientes con useState

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 const UseState = () => {
+  const SECURITY_CODE = 'ultrasecreto';
+  const [value, setValue] = useState('');
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -8,6 +10,9 @@ const UseState = () => {
     if (!!loading) {
       console.log('Iniciando comprobación');
       setTimeout(() => {
+        if (value !== SECURITY_CODE) {
+          setError(true);
+        }
         setLoading(false);
       }, 3000);
       console.log('Finalizando Comprobación');
@@ -19,11 +24,25 @@ const UseState = () => {
       <h2>Eliminar UseState</h2>
       <p>Código de seguridad</p>
       {error && <p>Error: el código es incorrecto</p>}
-      <input type="text" placeholder="Código de Seguridad" />
-      <button onClick={() => setError(!error)}>Comprobar</button>
+      <input
+        type="text"
+        placeholder="Código de Seguridad"
+        value={value}
+        onChange={(e) => {
+          // setError(false);
+          setValue(e.target.value);
+        }}
+      />
 
       {loading && <p>Cargando ....</p>}
-      <button onClick={() => setLoading(!loading)}>Cargar</button>
+      <button
+        onClick={() => {
+          setLoading(!loading);
+          setError(false);
+        }}
+      >
+        Comprobar
+      </button>
     </div>
   );
 };
