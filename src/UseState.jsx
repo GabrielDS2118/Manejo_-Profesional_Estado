54b64b16979b0823628698cd29b827510ecf27f1
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-03-27 17:22:16 -0500

    8.Estados compuestos con useState

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -2,18 +2,35 @@ import React, { useEffect, useState } from 'react';
 
 const UseState = () => {
   const SECURITY_CODE = 'ultrasecreto';
-  const [value, setValue] = useState('');
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+
+  const [state, setState] = useState({
+    value: '',
+    error: false,
+    loading: false,
+  });
+
+  const { loading, error, value } = state;
+  // const [value, setValue] = useState('');
+  // const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!!loading) {
       console.log('Iniciando comprobación');
       setTimeout(() => {
-        if (value !== SECURITY_CODE) {
-          setError(true);
+        if (value === SECURITY_CODE) {
+          setState({
+            ...state,
+            error: false,
+            loading: false,
+          });
+        } else {
+          setState({
+            ...state,
+            error: true,
+            loading: false,
+          });
         }
-        setLoading(false);
       }, 3000);
       console.log('Finalizando Comprobación');
     }
@@ -30,15 +47,21 @@ const UseState = () => {
         value={value}
         onChange={(e) => {
           // setError(false);
-          setValue(e.target.value);
+          setState({
+            ...state,
+            value: e.target.value,
+          });
         }}
       />
 
       {loading && <p>Cargando ....</p>}
       <button
         onClick={() => {
-          setLoading(!loading);
-          setError(false);
+          setState({
+            ...state,
+            error: false,
+            loading: true,
+          });
         }}
       >
         Comprobar
