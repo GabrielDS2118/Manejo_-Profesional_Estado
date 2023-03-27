c50becc6cc41d0981859be7ea35959d594ae4429
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2023-03-27 15:54:04 -0500

    5.Métodos del ciclo de vida en React.Component

--- src/ClassState.jsx
+++ src/ClassState.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Loading } from './Loading';
 
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: false,
+      loading: false,
     };
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate() {
+    console.log('actualizacion');
+
+    if (!!this.state.loading) {
+      setTimeout(() => {
+        this.setState({ loading: false });
+      }, 3000);
+    }
   }
 
   render() {
@@ -18,6 +38,9 @@ class ClassState extends React.Component {
         <button onClick={() => this.setState({ error: !this.state.error })}>
           Comprobar
         </button>
+
+        {this.state.loading && <Loading />}
+        <button onClick={() => this.setState({ loading: true })}>Cargar</button>
       </div>
     );
   }
